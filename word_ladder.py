ed096ed29d18d2b178e5c0c26a56580a0d051e4d
ekurz20 <ekurz20@cmc.edu>
2020-03-08 16:47:15 -0700

    added classes

--- word_ladder.py
+++ word_ladder.py
@@ -1,5 +1,40 @@
 #!/bin/python3
 
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def enqueue(self,item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self,item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+
+    def size(self):
+        return len(self.items)
+
 
 def word_ladder(start_word, end_word, dictionary_file='words5.dict'):
     '''
